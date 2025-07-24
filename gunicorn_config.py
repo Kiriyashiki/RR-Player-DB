d25bf53c71874ac41c5c53043b73b36be8766fa3
Kiriyashiki <42336759+Kiriyashiki@users.noreply.github.com>
2025-07-24 12:12:59 +0200

    capture logs

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -7,6 +7,9 @@ threads = int(os.environ.get('GUNICORN_THREADS', '4'))
 bind = os.environ.get('GUNICORN_BIND',
                       '{0}:{1}'.format(os.getenv('FLASK_HOST', '127.0.0.1'), os.getenv('FLASK_PORT', '8338')))
 
+accesslog = '-'  # stdout
+errorlog = '-'   # stderr
+capture_output = True
 
 forwarded_allow_ips = '*'
 
